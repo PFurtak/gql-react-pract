e76f6e09bfd23d6bd4936365b615def44020433e
Patrick Furtak <patrick@patrickfurtak.com>
2020-12-10 15:53:21 -0500

    Brought in semantic ui, created navigation and placeholder pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import MenuBar from '../src/components/layout/MenuBar';
+import Home from '../src/components/pages/Home';
+import Login from '../src/components/pages/Login';
+import Register from '../src/components/pages/Register';
 
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+      </Container>
+    </Router>
   );
 }
 

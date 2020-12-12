5fa058385e1e2d9d08e6cbcf94d3d5758d9ac98b
Patrick Furtak <patrick@patrickfurtak.com>
2020-12-11 22:28:07 -0500

    Fixed app crash on gql fetch, code was trying to destructure the response before it existed, giving it a default value of an empty object fixes the bug

--- client/src/components/posts/PostCard.jsx
+++ client/src/components/posts/PostCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import moment from 'moment';
 
 function PostCard({
-  post: { body, createdAt, id, username, likes, comments },
+  post: { body, createdAt, id, username, comments, likes },
 }) {
   function likePost() {
     console.log('Like post!!');
